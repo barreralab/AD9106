5e70ce7028baf9e11e292d7d0c7f1d554aedb7bc
Vishwanath Wimalasena <vishy235@gmail.com>
2024-05-17 13:03:00 -0400

    initial commit

---
+++ AD9106.cpp
@@ -0,0 +1,105 @@
+#include "Arduino.h"
+
+#include <SPI.h>
+#include "AD9106.h"
+
+AD9106::AD9106(int CS, int RESET, int TRIGGER, int EN_CVDDX)
+    : cs(CS), reset(RESET), _trigger(TRIGGER), _en_cvddx(EN_CVDDX) {}
+
+/*
+ * @brief Initialize GPIO and SPI pins on Arduino
+ * @param none
+ * @return none
+ */
+void AD9106::begin() {
+  pinMode(cs, OUTPUT);
+  pinMode(reset, OUTPUT);
+  pinMode(_trigger, OUTPUT);
+  pinMode(_en_cvddx, OUTPUT);
+
+  digitalWrite(cs, HIGH);
+  digitalWrite(_trigger, HIGH);
+  digitalWrite(reset, HIGH);
+
+  // Enable on-board oscillators
+  digitalWrite(_en_cvddx, HIGH);
+}
+
+void AD9106::reg_reset() {
+  digitalWrite(reset, LOW);
+  delay(10);
+  digitalWrite(reset, HIGH);
+}
+
+/*
+ * @brief Start pattern generation by setting AD9106 trigger pin to 0
+ * @param none
+ * @return none
+ */
+void AD9106::start_pattern() {
+  digitalWrite(_trigger, LOW);
+}
+
+/*
+ * @brief Stop pattern generation by setting AD9106 trigger pin to 1
+ * @param none
+ * @return none
+ */
+void AD9106::stop_pattern() {
+  digitalWrite(_trigger, HIGH);
+}
+
+/*********************************************************/
+// SPI FUNCTIONS
+/*********************************************************/
+
+/*
+ * @brief Initialize AD9106 SPI communication at [hz] speed
+ * @param hz - SPI bus frequency in hz
+ * @return none
+ */
+void AD9106::spi_init(uint32_t hz) {
+  SPI.begin();
+  SPI.beginTransaction(SPISettings(hz, MSBFIRST, SPI_MODE0));
+}
+
+/*
+ * @brief Write 16-bit data to AD9106 SPI/SRAM register
+ * @param addr - SPI/SRAM address
+ * @param data - data to be written to register address
+ * @return none
+ */
+int16_t AD9106::spi_write(uint16_t addr, int16_t data) {
+  int16_t out;
+
+  digitalWrite(cs, LOW);
+
+  SPI.transfer16(addr);
+  out = SPI.transfer16(data);
+
+  digitalWrite(cs, HIGH);
+  delay(1);
+
+  return out;
+};
+
+/*
+ * @brief Read 16-bit data from AD9106 SPI/SRAM register
+ * @param addr - SPI/SRAM address
+ * @return reg_data - data returned by AD9106
+ */
+int16_t AD9106::spi_read(uint16_t addr) {
+  digitalWrite(cs, LOW);
+
+  uint16_t read_add;
+  int16_t out;
+
+  read_add = addr | 0x8000;
+  SPI.transfer16(read_add);
+  out = SPI.transfer16(0);
+
+  digitalWrite(cs, HIGH);
+  delay(1);
+
+  return out;
+}
---
+++ AD9106.h
@@ -0,0 +1,71 @@
+/******************************************************************************
+    @file:  ad9106.h
+
+    @brief: Library for EVAL-AD9106 DAC arbitrary wave-form generator designed
+            to work as header for Arduino UNO R3
+******************************************************************************/
+
+#ifndef __ad910x_h__
+#define __ad910x_h__
+
+#include "Arduino.h"
+
+class AD9106 {
+ public:
+  int cs;
+  int reset;
+
+  /*** SPI register addresses ***/
+  uint16_t reg_add[66] = {
+      0x0000, 0x0001, 0x0002, 0x0003, 0x0004, 0x0005, 0x0006, 0x0007, 0x0008,
+      0x0009, 0x000a, 0x000b, 0x000c, 0x000d, 0x000e, 0x001f, 0x0020, 0x0022,
+      0x0023, 0x0024, 0x0025, 0x0026, 0x0027, 0x0028, 0x0029, 0x002a, 0x002b,
+      0x002c, 0x002d, 0x002e, 0x002f, 0x0030, 0x0031, 0x0032, 0x0033, 0x0034,
+      0x0035, 0x0036, 0x0037, 0x003e, 0x003f, 0x0040, 0x0041, 0x0042, 0x0043,
+      0x0044, 0x0045, 0x0047, 0x0050, 0x0051, 0x0052, 0x0053, 0x0054, 0x0055,
+      0x0056, 0x0057, 0x0058, 0x0059, 0x005a, 0x005b, 0x005c, 0x005d, 0x005e,
+      0x005f, 0x001e, 0x001d};
+
+  /*** 4-Wire SPI, Reset, and Trigger configuration & constructor ***/
+  AD9106(int CS = 10, int RESET = 8, int TRIGGER = 7, int EN_CVDDX = 2);
+
+  // Function to initialize GPIO pins on Arduino
+  void begin();
+
+  // Function to reset register values
+  void reg_reset();
+
+  // Function to start pattern generation
+  void start_pattern();
+
+  // Function to stop pattern generation
+  void stop_pattern();
+
+  // Function to setup SPI with communication speed of [hz]
+  void spi_init(uint32_t hz);
+
+  // SPI write function
+  int16_t spi_write(uint16_t addr, int16_t data);
+
+  // SPI read function
+  int16_t spi_read(uint16_t addr);
+
+  // // Function to display register data
+  // void print_data(uint16_t addr, uint16_t data);
+
+  // // Function to write to SRAM
+  // void AD910x_update_sram(int16_t data[]);
+
+  // // Function to display n SRAM data
+  // void AD910x_print_sram(uint16_t n);
+
+  // // Function to write to device SPI registers and display updated register
+  // // values
+  // void AD910x_update_regs(uint16_t data[]);
+
+ private:
+  int _en_cvddx;
+  int _trigger;
+};
+
+#endif
